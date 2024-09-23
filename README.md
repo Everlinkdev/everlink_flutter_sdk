090da763e6d0d25e70aa36eec0336a394a9ef6ee
Nathan Lewis <112nathan112@gmail.com>
2024-09-23 03:27:10 +0100

    Added content to readme

--- README.md
+++ README.md
@@ -1,4 +1,151 @@
-# everlink_sdk
+# Everlink Flutter SDK
 
-## Getting Started
+Allows apps developed using Flutter to use Everlink’s native SDKs to enable proximity verification via ultrasound.
 
+## Installation
+
+### Android
+
+- Edit `android/build.gradle` to look like this
+
+    ```diff
+    allprojects {
+        repositories {
+            ...
+            google()
+            mavenCentral()
+    +       maven {
+    +           url "https://repo.everlink.co/repository/maven-releases/"
+    +       }
+        }
+    }
+    ```
+
+## Usage
+
+- Import `everlink_sdk.dart`
+       
+   ```dart
+   import 'package:everlink_sdk/everlink_sdk.dart';
+   ```
+
+- Initialize EverlinkSdk class passing it your **appID key**
+
+   ```dart
+   final everlinkSdk = EverlinkSdk("your appID key");
+   ```
+
+- Set up event listener
+
+  ```dart
+  static const eventChannel = EventChannel('everlink_sdk_event');
+  ...
+      eventChannel.receiveBroadcastStream().listen((dynamic event) {
+      try {
+        final parsedJson = jsonDecode(event.toString());
+        var msgType = parsedJson['msg_type'];
+        var data = parsedJson['data'];
+
+        switch (msgType) {
+          case 'generated_token':
+            //a new token generated, to save in your database
+            var oldToken = data['old_token'];
+            var newToken = data['new_token'];
+            break;
+          case 'detection':
+            //you can now identify via the returned token what location/device was heard
+            var detectedToken = data['token'];
+          default:
+        }
+      } on Exception catch (e) {
+        print('Unknown exception: $e');
+      } catch (e) {
+        // No specified type, handles all
+        print('Something really unknown: $e');
+      }
+      //  print("event from native code $event");
+    }, onError: (dynamic error) {
+      print("Error: $error");
+    });
+  ```
+
+- Detect code
+  ```dart
+  Future<void> everlinkStartDetecting() async {
+  everlinkSdk.startDetecting();
+  }
+  Future<void> everlinkStopDetecting() async {
+  everlinkSdk.stopDetecting();
+  }
+  ```
+  
+  *Note users will be prompted to grant microphone permission*
+
+  On successful detection we will return the identifying token of the heard device via the **everlink_sdk_event** EventChannel broadcast stream.
+
+  ‍You can now search your database or locally using the returned Everlink unique identifying token to find the detected user. You might have some code like this:
+
+  ```sql
+  "SELECT * FROM employees WHERE everlink_token = token";
+  }
+  ```
+
+- Send code
+  ```dart
+  Future<void> everlinkStartEmitting() async {
+    everlinkSdk.startEmitting();
+  }
+  Future<void> everlinkStopEmitting() async {
+    everlinkSdk.stopEmitting();
+  }
+  ```
+
+  This will cause the device to start emitting the audiocode of the latest token generated on the device.
+
+  You can alternatively pass a token as an argument and its audiocode will play, as shown below:
+
+  ```dart
+  Future<void> everlinkStartEmittingToken(String token) async {
+    everlinkSdk.startEmittingToken(token);
+  }
+  ```
+
+- Volume settings
+
+  Function playVolume(volume, loudspeaker) allows you to set the volume and whether the audio should default to the loudspeaker.
+
+  ```dart
+  Future<void> everlinkPlayVolume(double volume, bool loudSpeaker) async {
+    everlinkSdk.playVolume(volume, loudSpeaker);
+  }
+  ```
+
+  We can detect if headphones are in use, and route the audio to the device’s loud speaker. Though users might experience a short pause in any audio they are listening to, while the audiocode is played, before we automatically resume playing what they were listening to before the interruption.
+
+- Create token
+
+  If you wish to manually generate a new user token *Otherwise one will be automatically generated.*
+
+  ```dart
+  Future<void> everlinkNewToken(String date) async {
+    everlinkSdk.newToken(date);
+  }
+  ```
+
+  On successful detection we will return the identifying token of the heard device via the **everlink_sdk_event** EventChannel broadcast stream.
+
+  *Function newToken(startDate) takes a validity start date in the form 'YYYY-MM-DD’. The token will be valid for two weeks after this date. If no validity date is provided then it will be the current date.  Once a device token is expired it will automatically refresh.*
+
+- Downloading tokens (needed if you want the SDK to work offline)
+
+  ```dart
+  Future<void> everlinSaveTokens(List<String> tokens) async {
+    everlinkSdk.saveTokens(tokens);
+  }
+  ```
+
+  For situations where it is possible to download your users' tokens prior to detecting, we strongly recommend that you do. This will reduce latency and make the verification faster and more reliable.
+
+---
+
+To learn more, **[read this](https://developer.everlink.co/developer-documention/android)**.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,152 +16,159 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => MainApp();
 }
 
-
-class MainApp extends State<MyApp>  {
-
-    @override
+class MainApp extends State<MyApp> {
+  @override
   void initState() {
     super.initState();
     setEvents();
   }
 
-    final everlinkSdk = EverlinkSdk("testAppID");
+  final everlinkSdk = EverlinkSdk("testAppID");
 
-    Future<void> everlinkStartDetecting() async {
-      everlinkSdk.startDetecting();
-    }
-    Future<void> everlinkStopDetecting() async {
-      everlinkSdk.stopDetecting();
-    }
-    Future<void> everlinkNewToken(String date) async {
-      everlinkSdk.newToken(date);
-    }
-    Future<void> everlinSaveTokens(List<String> tokens) async {
-      everlinkSdk.saveTokens(tokens);
-    }
-    Future<void> everlinkClearTokens() async {
-      everlinkSdk.clearTokens();
-    }
-    Future<void> everlinkStartEmitting() async {
-      everlinkSdk.startEmitting();
-    }
-    Future<void> everlinkStartEmittingToken(String token) async {
-      everlinkSdk.startEmittingToken(token);
-    }
-    Future<void> everlinkStopEmitting() async {
-      everlinkSdk.stopEmitting();
-    }
-    Future<void> everlinkPlayVolume(double volume, bool loudSpeaker) async {
-      everlinkSdk.playVolume(volume, loudSpeaker);
-    }
+  Future<void> everlinkStartDetecting() async {
+    everlinkSdk.startDetecting();
+  }
 
-static const eventChannel = EventChannel('everlink_sdk_event');
+  Future<void> everlinkStopDetecting() async {
+    everlinkSdk.stopDetecting();
+  }
 
+  Future<void> everlinkNewToken(String date) async {
+    everlinkSdk.newToken(date);
+  }
 
-  void setEvents() { 
+  Future<void> everlinSaveTokens(List<String> tokens) async {
+    everlinkSdk.saveTokens(tokens);
+  }
+
+  Future<void> everlinkClearTokens() async {
+    everlinkSdk.clearTokens();
+  }
+
+  Future<void> everlinkStartEmitting() async {
+    everlinkSdk.startEmitting();
+  }
+
+  Future<void> everlinkStartEmittingToken(String token) async {
+    everlinkSdk.startEmittingToken(token);
+  }
+
+  Future<void> everlinkStopEmitting() async {
+    everlinkSdk.stopEmitting();
+  }
+
+  Future<void> everlinkPlayVolume(double volume, bool loudSpeaker) async {
+    everlinkSdk.playVolume(volume, loudSpeaker);
+  }
+
+  static const eventChannel = EventChannel('everlink_sdk_event');
+
+  void setEvents() {
     eventChannel.receiveBroadcastStream().listen((dynamic event) {
       try {
-      final parsedJson = jsonDecode(event.toString());
-      var msgType = parsedJson['msg_type'];
-     // var data = parsedJson['data'];
+        final parsedJson = jsonDecode(event.toString());
+        var msgType = parsedJson['msg_type'];
+        var data = parsedJson['data'];
 
-      switch (msgType) {
-        case 'newToken':
-          
-          break;
-        default:
-      }
+        switch (msgType) {
+          case 'generated_token':
+            //extract both and send to function
+            var oldToken = data['old_token'];
+            var newToken = data['new_token'];
+            break;
+          case 'detection':
+            //send token to function
+            var detectedToken = data['token'];
+          default:
+        }
       } on Exception catch (e) {
-  print('Unknown exception: $e');
-} catch (e) {
-  // No specified type, handles all
-  print('Something really unknown: $e');
-}
-      print("*****Event from native****** $event");
-      print(jsonDecode(jsonEncode(event.toString())));
+        print('Unknown exception: $e');
+      } catch (e) {
+        // No specified type, handles all
+        print('Something really unknown: $e');
+      }
+      //  print("event from native code $event");
     }, onError: (dynamic error) {
       print("Error: $error");
     });
-
   }
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-           appBar: AppBar(
-              backgroundColor: Colors.green,
-              title: const Text('Test'),
-            ),
-      body: Column(
-        children: [
-          const Text('Everlink Plugin'),
-          ElevatedButton(
-            onPressed: () {
-              everlinkStartDetecting();
-            },
-            child: const Text('Start Detecting'),
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: const Text('Test'),
           ),
-          ElevatedButton(
-            onPressed: () {
-              everlinkStopDetecting();
-            },
-            child: const Text('Stop Detecting'),
+          body: Column(
+            children: [
+              const Text('Everlink Plugin'),
+              ElevatedButton(
+                onPressed: () {
+                  everlinkStartDetecting();
+                },
+                child: const Text('Start Detecting'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  everlinkStopDetecting();
+                },
+                child: const Text('Stop Detecting'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  const date = "2024";
+                  everlinkNewToken(date);
+                },
+                child: const Text('New Token'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  const tokensList = [
+                    'evpan1d9d38808c0dc626543920c58e9d903c',
+                    'evpan9823a9bbe65b0ff54968d4638a55e352'
+                  ];
+                  everlinSaveTokens(tokensList);
+                },
+                child: const Text('Save Tokens'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  everlinkClearTokens();
+                },
+                child: const Text('Clear Tokens'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  everlinkStartEmitting();
+                },
+                child: const Text('Start Emitting'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  const token = "evpan9823a9bbe65b0ff54968d4638a55e352";
+                  everlinkStartEmittingToken(token);
+                },
+                child: const Text('Start Emitting Token'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  everlinkStopEmitting();
+                },
+                child: const Text('Stop Emitting'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  const volume = 0.9;
+                  const useLoudSpeaker = true;
+                  everlinkPlayVolume(volume, useLoudSpeaker);
+                },
+                child: const Text('Play Volume'),
+              ),
+            ],
           ),
-           ElevatedButton(
-            onPressed: () {
-              const date = "2024";
-              everlinkNewToken(date);
-            },
-            child: const Text('New Token'),
-          ),
-           ElevatedButton(
-            onPressed: () {
-              const tokensList = [ 'evpan1d9d38808c0dc626543920c58e9d903c', 'evpan9823a9bbe65b0ff54968d4638a55e352' ];
-              everlinSaveTokens(tokensList);
-
-            },
-            child: const Text('Save Tokens'),
-          ),
-           ElevatedButton(
-            onPressed: () {
-              everlinkClearTokens();
-            },
-            child: const Text('Clear Tokens'),
-          ),
-            ElevatedButton(
-            onPressed: () {
-              everlinkStartEmitting();
-            },
-            child: const Text('Start Emitting'),
-          ),
-            ElevatedButton(
-            onPressed: () {
-              const token = "evpan9823a9bbe65b0ff54968d4638a55e352";
-              everlinkStartEmittingToken(token);
-            },
-            child: const Text('Start Emitting Token'),
-          ),
-                    ElevatedButton(
-            onPressed: () {
-              everlinkStopEmitting();
-            },
-            child: const Text('Stop Emitting'),
-          ),
-                    ElevatedButton(
-            onPressed: () {
-              const volume = 0.9;
-              const useLoudSpeaker = true;
-              everlinkPlayVolume(volume, useLoudSpeaker);
-            },
-            child: const Text('Play Volume'),
-          ),
-        ],
-      ),
-    ));
-  
+        ));
+  }
 }
-}
-
